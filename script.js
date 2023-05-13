f4f39388d3e08f4f68cfce389a015a9ea85df233
Ayush Raj Singh <114082841+AyushRajSingh30@users.noreply.github.com>
2023-05-14 02:02:53 +0530

    JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+ let a=document.getElementById("btn001").addEventListener("click",function(a){
+    window.location.href='https://code.visualstudio.com/docs/?dv=linux64_deb';
+ })
+
+
+let b=document.getElementById("btn002").addEventListener("click",function(a){
+    window.location.href='https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions';
+})
